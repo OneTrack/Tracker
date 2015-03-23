421efcb621b02754695131fe5d96ad83bc3bb043
himanshuonetrack <himanshusbindia@gmail.com>
2015-03-24 00:37:38 +0530

    codeigniter added
    framework added + database config added + various encryptions keys added

---
+++ application/views/test_view.php
@@ -0,0 +1,32 @@
+<h>Registration</h>
+<form method='post' action='/user/registration'/>
+<p>
+<input type='text' placeholder='Email' name='email'/>
+</p>
+<p>
+<input type='text' placeholder='Name' name='name'/>
+</p>
+<p>
+<input type='password' placeholder='Password' name='password'/>
+</p>
+<p>
+<input type='password' placeholder='Confirm Password' name='confirm_password'/>
+</p>
+<p>
+<input type='submit' value='Submit'/>
+</p>
+</form>
+
+
+<h>Login</h>
+<form method='post' action='/user/login'/>
+<p>
+<input type='text' placeholder='Email' name='email'/>
+</p>
+<p>
+<input type='password' placeholder='Password' name='password'/>
+</p>
+<p>
+<input type='submit' value='Submit'/>
+</p>
+</form>
